9ee1bceb7b0c6ed66e4e8d9fad3af78ee4d188fc
vs-cqp <106712504+AlanaOlive@users.noreply.github.com>
2024-06-27 18:01:14 -0300

    atualizado menu de projetos

---
+++ scripts/backToTop.js
@@ -0,0 +1,22 @@
+
+document.addEventListener('DOMContentLoaded', function(){
+    var backToTopButton = document.getElementById('back-to-top-btn');
+
+    window.addEventListener('scroll', function() {
+        if (window.scrollY > 300) { 
+            backToTopButton.style.display = 'block';
+        } else {
+            backToTopButton.style.display = 'none';
+        }
+    });
+
+    backToTopButton.addEventListener('click', function() {
+        window.scrollTo({
+            top: 0,
+            behavior: 'smooth' 
+        });
+    });
+
+});
+
+
